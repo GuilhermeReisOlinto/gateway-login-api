fecd306bcd70d4fff822cbb0b645d05718d49787
guilherme olinto <ctl_golinto@uolinc.com>
2025-02-28 15:12:41 -0300

    feat: create authenticate middleware

--- public/Routes.php
+++ public/Routes.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Authentication\HandlerTokenJwt;
+use App\Authentication\MiddlewareToken;
 use App\HandlerCustomer\HandlerCustomerController;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
 $app = AppFactory::create();
+
+$jwtHandler = new HandlerTokenJwt();
+$app->add(new MiddlewareToken($jwtHandler));
 
 $app->post("/", function(Request $request, Response $response, $arg) {
     return $response->withHeader('Content-Type', 'application/json')
@@ -14,6 +19,8 @@ $app->post("/", function(Request $request, Response $response, $arg) {
 
 $app->post("/customer", HandlerCustomerController::class . ':test');
 $app->get("/customer", [HandlerCustomerController::class, 'test']);
+
+
 $app->get('/customer/{documentNumber}', [HandlerCustomerController::class, 'test']);
 
 $app->run();
