8b97762edc85d4100b6587421078a975aad66c5a
guilherme olinto <ctl_golinto@uolinc.com>
2025-03-06 14:17:41 -0300

    feat: update dockerfile and index file

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ $dependence = DependenciesFactory::create();
 $app = $dependence['app'];
 $consumer = $dependence['consumer'];
 
-$server = new Server('0.0.0.0', 9000);
+$server = new Server('0.0.0.0', 9001);
 
 $server->on('start', function (Server $server) {
     echo "Servidor HTTP rodando em http://0.0.0.0:9501\n";
@@ -33,14 +33,12 @@ $server->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $s
 $server->on('workerStart', function (Server $server, int $workerId) use ($consumer) {
     echo "Worker $workerId iniciado\n";
 
-    // Loop para consumir mensagens do Kafka
-    if ($workerId === 0) { // Apenas um worker consome do Kafka
+    if ($workerId === 0) {
         while (true) {
-            $message = $consumer->consume(1000); // Timeout de 1 segundo
+            $message = $consumer->consume(1000);
 
             if ($message->err === RD_KAFKA_RESP_ERR_NO_ERROR) {
                 echo "Mensagem recebida: " . $message->payload . "\n";
-                // Aqui você pode processar a mensagem
             }
         }
     }
