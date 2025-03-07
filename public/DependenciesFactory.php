4e8517b8221565d73515d22279a5185eb84e6697
guilherme olinto <ctl_golinto@uolinc.com>
2025-03-07 13:29:25 -0300

    feat:update files

--- public/DependenciesFactory.php
+++ public/DependenciesFactory.php
@@ -6,6 +6,11 @@ require __DIR__ . "/ConsumerKafka.php";
 use App\Authentication\HandlerTokenJwt;
 use App\Authentication\MiddlewareToken;
 use Slim\Factory\AppFactory;
+use Swoole\Http\Server;
+use Swoole\Http\Request as SwooleRequest;
+use Swoole\Http\Response as SwooleResponse;
+use Swoole\Coroutine;
+use GuzzleHttp\Psr7\ServerRequest;
 
 class DependenciesFactory 
 {
@@ -34,5 +39,48 @@ class DependenciesFactory
             'kafka' => $kafka,
             'consumer' => $consumer
         ];
+    }
+
+    public static function server($app, $consumer)
+    {
+
+        $server = new Server('0.0.0.0', 9501);
+
+        $server->on('start', function (Server $server) {
+            echo "Servidor HTTP rodando em http://0.0.0.0:9501\n";
+        });
+        
+        $server->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) use ($app) {
+            $request = ServerRequest::fromGlobals();
+        
+            $response = $app->handle($request);
+        
+            foreach ($response->getHeaders() as $name => $values) {
+                $swooleResponse->header($name, implode(', ', $values));
+            }
+            $swooleResponse->status($response->getStatusCode());
+            $swooleResponse->end($response->getBody());
+        });
+       
+        $server->on('workerStart', function (Server $server, int $workerId) use ($consumer) {
+            echo "Worker $workerId iniciado\n";
+        
+            if ($workerId === 0) {
+        
+                Coroutine::create(function () use ($consumer) {
+                    while (true) {
+                        $message = $consumer->consume(100);
+        
+                        if ($message->err === RD_KAFKA_RESP_ERR_NO_ERROR) {
+                            echo "Mensagem recebida: " . $message->payload . "\n";
+                        }
+                        
+                        Coroutine::sleep(0.01);
+                    }
+                });
+            }
+        });
+        
+        return $server;
     }
 }
